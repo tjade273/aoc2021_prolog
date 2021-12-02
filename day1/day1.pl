ffd856cd3c5f4ded7f22b18b2eaf3b55a62995cf
Tjaden Hess <tjade273@gmail.com>
2021-12-01 21:02:47 -0600

    Switch to CPL(FD) for no good reason, except fun

--- day1/day1.pl
+++ day1/day1.pl
@@ -2,16 +2,19 @@
 
 :- use_module(library(pio)).
 :- use_module(library(dcg/basics)).
+:- use_module(library(clpfd)).
+
 :- initialization main.
 
 measurements([H|T]) --> digits(H), "\n", measurements(T).
 measurements([]) --> [].
 
-increases(X, [H1,H2|T]) :- H2 > H1, increases(Y, [H2|T]), X is Y + 1, !.
-increases(X, [_|T]) :- increases(X, T), !.
-increases(0, _).
+increases(X, [H1,H2|T]) :- H2 #> H1, increases(Y, [H2|T]), X #= Y + 1, !.
+increases(X, [_,H2|T]) :- increases(X, [H2|T]).
+increases(0, [_]).
+increases(0, []).
 
-window_sum([X|Y], [H1,H2,H3|T]) :- X is H1 + H2 + H3, window_sum(Y, [H2,H3|T]), !.
+window_sum([X|Y], [H1,H2,H3|T]) :- X #= H1 + H2 + H3, window_sum(Y, [H2,H3|T]), !.
 window_sum([], _).
 
 main :- 
