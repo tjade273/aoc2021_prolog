48598dec22da32a5cc5e02a7358712e3159d7c0f
Tjaden Hess <tjade273@gmail.com>
2021-12-09 10:24:28 -0600

    Day9 Part 2

--- day9/day9.pl
+++ day9/day9.pl
@@ -6,6 +6,7 @@
 :- use_module(library(clpfd)).
 
 :- initialization main.
+:- table flows_into/3.
 
 % Input Parsing
 
@@ -14,13 +15,10 @@ row(R) --> sequence(depth, R), "\n".
 grid(G) --> sequence(row, G).
 
 parse_input(Grid) :- phrase_from_file(grid(Grid), 'input.txt').
-%parse_input(Grid) :- phrase_from_file(grid(Grid), 'test.txt').
 
 % Part 1
 
 % Convert the grid to an association list of index -> value mappings. 
-% This speeds the overall runtime from O(N^2) to O(NlogN).
-
 % Tail-recursive for funzies
 grid_to_assoc_([], _, _, Assoc, Assoc).
 
@@ -42,16 +40,22 @@ neighbor((I, J0), (I, J1)) :-
 neighbor((I0, J), (I1, J)) :-
     abs(I1 - I0) #= 1.
 
-has_low_neighbor(Grid, Index) :-
-    get_assoc(Index, Grid, Self), 
+lower_neighbor(Grid, Index, (I1, J1)) :-
+    get_assoc(Index, Grid, Self),
     neighbor(Index, (I1, J1)),
     label([I1, J1]),
     get_assoc((I1, J1), Grid, Neighbor),
-    Self #>= Neighbor.
+    Neighbor #< Self.
     
 local_minimum(Grid, Minimum) :-
     gen_assoc(Index, Grid, Minimum),
-    \+ has_low_neighbor(Grid, Index).
+    \+ lower_neighbor(Grid, Index, _).
+
+flows_into(Grid, Index, Index) :- get_assoc(Index, Grid, 9), !.
+flows_into(Grid, Index, LocalMin) :-
+    lower_neighbor(Grid, Index, IndexLower), !,
+    flows_into(Grid, IndexLower, LocalMin).
+flows_into(_, LocalMin, LocalMin).
 
 part1(Grid, N) :-
     findall(Minimum, local_minimum(Grid, Minimum), Mins),
@@ -59,9 +63,23 @@ part1(Grid, N) :-
     sum_list(Mins, S),
     N #= L + S.
 
+swap(X-Y, Y-X).
+
+part2(Grid, N) :-
+    assoc_to_keys(Grid, Ids),
+    maplist(flows_into(Grid), Ids, Minima),
+    msort(Minima, Sorted),
+    clumped(Sorted, Clumped), 
+    maplist(swap, Clumped, Swapped),
+    msort(Swapped, SwapSort),
+    reverse(SwapSort, [X1-_, X2-_, X3-_ | _]),
+    N #= X1 * X2 * X3.
+
 main :-
     parse_input(Grid),
     grid_to_assoc(Grid, Assoc),
     part1(Assoc, N1),
+    part2(Assoc, N2),
     print(N1), nl,
+    print(N2), nl,
     halt(0).
