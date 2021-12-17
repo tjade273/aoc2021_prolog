da40fab57560efa7d4f5a7a48024868e0708232c
Tjaden Hess <tjade273@gmail.com>
2021-12-17 14:17:39 -0600

    Day16 Part 1

---
+++ day16/day16.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/env swipl
+
+:- use_module(library(readutil)).
+:- use_module(library(dcg/basics)).
+:- use_module(library(dcg/high_order)).
+:- use_module(library(clpfd)).
+
+%:- set_prolog_flag(double_quotes, chars).
+
+%:- initialization main.
+
+% (Hex) Input Parsing
+
+
+input(N) --> xinteger(N), "\n".
+parse_input(File, S) :-
+    phrase_from_file(input(N), File),
+    format(codes(S), "~2r", [N]).
+
+% Packet Parsing
+
+binary_number(Bs, N) :-
+   binary_number_min(Bs, 0,N, N).
+
+binary_number_min([], N,N, _M).
+binary_number_min([B|Bs], N0,N, M) :-
+   B in 0..1,
+   N1 #= B+2*N0,
+   M #>= N1,
+   binary_number_min(Bs, N1,N, M).
+
+bit(0) --> `0`.
+bit(1) --> `1`.
+bit_n(X) --> {member(X, [0, 1])}, [X].
+binary_number(N, Len) --> {length(Bits, Len)}, sequence(bit, Bits), {binary_number(Bits, N)}.
+
+packet_header(Version, TypeID) --> binary_number(Version, 3), binary_number(TypeID, 3). 
+
+lstop, [lstop] --> [].
+literal, Bits --> bit(0), {length(Bits, 4)}, sequence(bit, Bits), lstop.
+literal, Bits --> bit(1), {length(Bits, 4)}, sequence(bit, Bits), literal.
+literal(N) --> literal, sequence(bit_n, Bits), [lstop], {binary_number(Bits, N)}.
+
+
+stopat(0), [stopat] --> [].
+stopat(N), [X] --> [X], {N #> 0, N1 #= N - 1}, stopat(N1).
+
+subpackets(0, Packets) --> binary_number(PacketLen, 15), stopat(PacketLen), sequence(packet, Packets), [stopat].
+subpackets(1, Packets) --> binary_number(PacketLen, 11), {length(Packets, PacketLen)}, sequence(packet, Packets).
+
+packet_body(4, N) --> literal(N).
+packet_body(_, Packets) --> bit(LType), subpackets(LType, Packets).
+
+packet((Version, TypeID, Body)) --> packet_header(Version, TypeID), packet_body(TypeID, Body).
+
+zero(_) --> `0`.
+transmission(Packet) --> packet(Packet), sequence(zero, _).
+
+
+
+
+% Part 1
+part1((Version, 4, _), Version) :- !.
+part1((Version, _, Packets), N) :- 
+    maplist(part1, Packets, Ns),
+    sum_list(Ns, N0),
+    N #= N0 + Version.
+
+
+    
+
+
+
+
